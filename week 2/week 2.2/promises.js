afc9e5cbde247a652cd932d22a1de284e74f4a52
amritrajputt <vishal179singh@gmail.com>
2025-10-22 14:35:28 +0530

    promises question

---
+++ week 2/week 2.2/promises.js
@@ -0,0 +1,48 @@
+const { rejects } = require("assert")
+const { resolve } = require("path")
+
+// Q1>> Simple Promise
+
+// Write a function isEven(num) that returns a Promise.
+// Resolves with "Even number" if the number is even.
+// Rejects with "Odd number" if the number is odd.
+
+// Test it with .then() and .catch().
+// sol:
+
+// const isEven = (num) =>{
+//     return new Promise((resolve,reject)=>{
+//          if(num === undefined){
+//             return reject (new Error("num not passed"))
+//         }
+//          if(num % 2 == 0)  resolve("Even number");
+//     else reject("Odd number");
+        
+//     })
+// }
+
+// isEven()
+// .then((message)=>console.log(message))
+// .catch((err)=>console.log(err));
+
+
+// Q2>>> Delayed Message
+
+// Write a function delayedMessage(msg, seconds) that returns a Promise.
+// Resolves after seconds seconds with the message msg.
+// Example: delayedMessage("Hello World", 3) should log "Hello World" after 3 seconds.
+
+const delayedMessage = (msg, seconds) =>{
+    return new Promise((resolve,reject)=>{
+        if(msg && seconds){
+        setTimeout(() => {
+           resolve (msg);
+        }, seconds*1000);
+    }else{
+        return reject(new Error("pass msg and seconds"))
+    }
+    })
+}
+delayedMessage("amrit",2)
+.then((message)=>console.log(message))
+.catch((err)=>console.log(err))
