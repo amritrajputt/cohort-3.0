12710bbd3aa67a18255aaf3ef74a9d8a5e4230a6
amritrajputt <vishal179singh@gmail.com>
2025-11-02 19:54:34 +0530

    file based todo backend done✅

--- week 4/node js/file based todo/todo.js
+++ week 4/node js/file based todo/todo.js
@@ -13,28 +13,83 @@ app.get('/', (req, res) => {
 
 //function to add to-do
 function addTodotofile(description, status) {
-    if(!description || !status){
+    if (!description || !status) {
         return new Error("Description and status are required")
     }
     const newTodo = {
-        todo:description,
-        todoStatus:status
+        todo: description,
+        todoStatus: status
     }
-    fs.readFile("todos.json","utf-8",(error,data)=>{
+    fs.readFile("todos.json", "utf-8", (error, data) => {
         let todos = []
-        if(!error && data){
-            try{
+        if (!error && data) {
+            try {
                 todos = JSON.parse(data)
-            }catch{
+            } catch {
                 todos = []
             }
         }
         todos.push(newTodo)
-        fs.writeFile("todos.json",JSON.stringify(todos,null,2), (err) => {
+        fs.writeFile("todos.json", JSON.stringify(todos, null, 2), (err) => {
             if (err) {
                 console.error("Error writing file:", err);
             }
+        })
     })
+}
+
+function deleteTodo(index) {
+    fs.readFile("todos.json", "utf-8", (err, data) => {
+        let todos = [];
+        if (!err && data) {
+            try {
+                todos = JSON.parse(data);
+            } catch {
+                todos = [];
+            }
+        }
+        if (typeof (index) !== "number" || index < 0 || index >= todos.length) {
+            console.error("Provide correct index");
+            return;
+        }
+        todos.splice(index, 1)
+        fs.writeFile("todos.json", JSON.stringify(todos, null, 2), (err) => {
+            if (err) {
+                console.error("Error writing file:", err);
+            }
+        })
+    })
+
+}
+
+function updateTodo(index, update) {
+    fs.readFile("todos.json", "utf-8", (err, data) => {
+        let todos = [];
+        if (!err && data) {
+            try {
+                todos = JSON.parse(data);
+            } catch {
+                todos = [];
+            }
+        }
+        if (typeof (index) !== "number" || index < 0 || index >= todos.length) {
+            console.error("Provide correct index");
+            return;
+        }
+        if (!update || typeof update !== "object") {
+            console.error("Updated todo object required");
+            return;
+        }
+
+        todos[index] = { ...todos[index], ...update };
+
+        fs.writeFile("todos.json", JSON.stringify(todos, null, 2), (err) => {
+            if (err) {
+                console.error("Error writing file:", err);
+            }
+        }
+        )
+
     })
 }
 app.listen(8000)
