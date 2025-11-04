c92b9c58bfa12d6c2c2993ca8401fbbbfbb1ec86
amritrajputt <vishal179singh@gmail.com>
2025-11-04 20:59:20 +0530

    week 4.4 middleware

--- week 4/week 4.4/middleware/index.js
+++ week 4/week 4.4/middleware/index.js
@@ -3,26 +3,26 @@ const app = express()
 
 // function to check age
 
-const checkAgeUsingMiddleware = (req,res,next) => {
+const checkAgeUsingMiddleware = (req, res, next) => {
     const age = req.query.age
-     if(age >= 14){
+    if (age >= 14) {
         next()
-    }else{
+    } else {
         res.json({
             msg: "sorry you are not of age yet",
         })
     }
 }
 
-app.get('/ride1',checkAgeUsingMiddleware,(req,res) => {
-        res.json({
+app.get('/ride1', checkAgeUsingMiddleware, (req, res) => {
+    res.json({
         msg: "you have successfully riden the ride 1"
-   })
+    })
 
 })
 
-app.get('/ride2',(req,res) => {
-        res.json({
+app.get('/ride2', (req, res) => {
+    res.json({
         mdg: "you have successfully riden the ride 2"
     })
 })
