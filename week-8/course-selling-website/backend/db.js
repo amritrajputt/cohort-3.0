c75eec55dae85f865051ce488471b479f45c173f
amritrajputt <vishal179singh@gmail.com>
2025-11-22 00:11:48 +0530

    basic schema for our db

--- week-8/course-selling-website/backend/db.js
+++ week-8/course-selling-website/backend/db.js
@@ -29,10 +29,23 @@ const courseSchema = new Schema({
     title: String,
     description: String,
     price: Number,
-    imageurl:String,
+    imageurl: String,
     creatorId: ObjectId
 })
 const purchaseSchema = new Schema({
-    courseId:ObjectId,
-    userId:ObjectId,
+    courseId: ObjectId,
+    userId: ObjectId,
 })
+
+const userModel = mongoose.model("user", userSchema)
+const adminModel = mongoose.model("admin", adminSchema)
+const courseModel = mongoose.model("course", courseSchema)
+const purchaseModel = mongoose.model("purchase", purchaseSchema)
+
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
