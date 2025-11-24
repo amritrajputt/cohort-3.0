b70e8c98c5ad1da1373d1c97cd79fa869de30b8e
amritrajputt <vishal179singh@gmail.com>
2025-11-24 21:07:24 +0530

    user signup endpoint ✅

--- week-8/course-selling-website/backend/routes/admin.js
+++ week-8/course-selling-website/backend/routes/admin.js
@@ -5,65 +5,65 @@ const bcrypt = require('bcrypt')
 const { z, email } = require('zod')
 
 
-    adminRouter.post('/signup', async (req, res) => {
+adminRouter.post('/signup', async (req, res) => {
 
-        const requiredInput = z.object({
-            email: z.string().min(6).max(60).email(),
-            password: z.string().min(8).max(20)
-                .refine((val) => [...val].some((c) => c => 'A' && c <= "Z"), {
-                    message: "Password must contain at least one uppercase letter",
-                })
-                .refine((val) => [...val].some((c) => c => 'a' && c <= "z"), {
-                    message: "Password must contain at least one lowercase letter",
-                })
-                .refine((value) => [...value].some((c) => c >= '0' && c <= '9'), {
-                    message: "Password must contain at least one number",
-                }),
-            firstname: z.string().min(3).max(30),
-            lastname: z.string().min(3).max(20)
+    const requiredInput = z.object({
+        email: z.string().min(6).max(60).email(),
+        password: z.string().min(8).max(20)
+            .refine((val) => [...val].some((c) => c => 'A' && c <= "Z"), {
+                message: "Password must contain at least one uppercase letter",
+            })
+            .refine((val) => [...val].some((c) => c => 'a' && c <= "z"), {
+                message: "Password must contain at least one lowercase letter",
+            })
+            .refine((value) => [...value].some((c) => c >= '0' && c <= '9'), {
+                message: "Password must contain at least one number",
+            }),
+        firstname: z.string().min(3).max(30),
+        lastname: z.string().min(3).max(20)
+    })
+    const parsedDataWithSuccess = requiredInput.safeParse(req.body)
+    if (!parsedDataWithSuccess.success) {
+        res.json({
+            message: "Incorrect format",
+            error: parsedDataWithSuccess.error
         })
-        const parsedDataWithSuccess = requiredInput.safeParse(req.body)
-        if (!parsedDataWithSuccess.success) {
-            res.json({
-                message: "Incorrect format",
-                error: parsedDataWithSuccess.error
-            })
-            return
+        return
+    }
+    const { email, firstname, lastname, password } = parsedDataWithSuccess.data
+    try {
+        const existing = await adminModel.findOne({ email })
+        if (existing) {
+            return res.status(400).json({ message: "User already exists" })
         }
-        const { email, firstname, lastname, password } = parsedDataWithSuccess.data
-        try {
-            const existing = await adminModel.findOne({ email })
-            if (existing) {
-                return res.status(400).json({ message: "User already exists" })
-            }
-            const hashedPassword = await bcrypt.hash(password, 10)
-            await adminModel.create({
-                firstname: firstname,
-                lastname: lastname,
-                email: email,
-                password: hashedPassword
-            })
-             res.json({ message: "You are signed up" })
-     } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message })
-}
-    })
+        const hashedPassword = await bcrypt.hash(password, 10)
+        await adminModel.create({
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            password: hashedPassword
+        })
+        res.json({ message: "You are signed up" })
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message })
+    }
+})
 
-    adminRouter.post('/login', (req, res) => {
+adminRouter.post('/login', (req, res) => {
 
-    })
+})
 
-    adminRouter.post('/course', (req, res) => {
+adminRouter.post('/course', (req, res) => {
 
-    })
+})
 
-    adminRouter.put('/course', (req, res) => {
+adminRouter.put('/course', (req, res) => {
 
-    })
+})
 
-    adminRouter.get('/course/bulk', (req, res) => {
+adminRouter.get('/course/bulk', (req, res) => {
 
-    })
+})
 
 module.exports = {
     adminRouter
